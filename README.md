fa2a1cb96e7ac63596039a6572383ea1dd50dd90
Sofiqur Rahman <sofiqurr849@gmail.com>
2020-11-11 04:35:46 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# loanCalculator
+# loan-calculator
---
+++ app.js
@@ -0,0 +1,61 @@
+document.getElementById('loan-form').addEventListener('submit',function(e){
+  // hide results
+  document.getElementById('results').style.display='none';
+  // show loading
+  document.getElementById('loading').style.display='block';
+  setTimeout(calculateResult,2000);
+  e.preventDefault();
+})
+
+function calculateResult(){
+  const amount=document.getElementById('amount');
+  const interest=document.getElementById('interest');
+  const years=document.getElementById('years');
+
+  const monthlyPayment=document.getElementById('monthly-payment');
+  const totalPayment=document.getElementById('total-payment');
+  const totalInterest=document.getElementById('total-interest');
+
+  const principal=parseFloat(amount.value);
+  const calculatedInterest=parseFloat(interest.value) / 100 /12;
+  const calculatedPayment=parseFloat(years.value) * 12;
+
+  const x=Math.pow(1 + calculatedInterest, calculatedPayment);
+  const monthly=(principal * x * calculatedInterest) / (x - 1);
+
+  if(isFinite(monthly)){
+    monthlyPayment.value=monthly.toFixed(2);
+    totalPayment.value=(monthly * calculatedPayment).toFixed(2);
+    totalInterest.value=((monthly * calculatedPayment) - principal).toFixed(2);
+
+    // show results
+    document.getElementById('results').style.display='block';
+    // hide loading
+    document.getElementById('loading').style.display='none';
+  } else{
+    showError('Please check the number');
+  }
+}
+
+function showError(error){
+  // hide results
+  document.getElementById('results').style.display='none';
+  // hide loading
+  document.getElementById('loading').style.display='none';
+  // create a div
+  const errorDiv=document.createElement('div');
+  // add class
+  errorDiv.className='alert alert-danger';
+  // create textnode and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+  
+  const card=document.querySelector('.card');
+  const heading=document.querySelector('.heading');
+  card.insertBefore(errorDiv,heading);
+  // clear error after 3 sec
+  setTimeout(clearError,3000);
+}
+
+function clearError(){
+  document.querySelector('.alert').remove();
+}
